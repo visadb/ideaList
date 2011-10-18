d7548c510f9685e16960421bf70ee6bd05959ae0
Visa Putkinen <visa.putkinen@iki.fi>
2011-10-18 16:10:49 +0300

    Remove obsolete import

--- ideaList/urls.py
+++ ideaList/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns
 #from django.views.generic import ListView
-#from ideaList.models import List
 from ideaList.views import main
 
 urlpatterns = patterns('',
