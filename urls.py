e445d14ad059710b40231f9bd941f9aeb754523a
Visa Putkinen <visa.putkinen@iki.fi>
2011-10-31 20:22:17 +0200

    Redirect from root to ideaList and retab url patterns.

--- urls.py
+++ urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^ideaList/',      include('ideaList.urls')),
-    url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
-    url(r'^passwd/$', 'django.contrib.auth.views.password_change'),
+    url(r'^$',          redirect_to, {'url': '/ideaList/', 'permanent': False}),
+    url(r'^ideaList/',  include('ideaList.urls')),
+    url(r'^login/$',    'django.contrib.auth.views.login'),
+    url(r'^logout/$',   'django.contrib.auth.views.logout_then_login'),
+    url(r'^passwd/$',   'django.contrib.auth.views.password_change'),
     url(r'^passwd_done/$', 'django.contrib.auth.views.password_change_done'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/',     include(admin.site.urls)),
 )
