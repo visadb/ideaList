615f19275a640e1bb4e8d23043980f2a7a5c0a72
Visa Putkinen <visa.putkinen@iki.fi>
2011-11-03 19:48:47 +0200

    Disable debug toolbar

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-INTERNAL_IPS = ('127.0.0.1',)
+INTERNAL_IPS = ('192.168.0.7',)
 
 SESSION_COOKIE_AGE = 15724800
 
@@ -108,7 +108,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'puhveli.urls'
@@ -128,7 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django_extensions',
-    'debug_toolbar',
+    #'debug_toolbar',
     'south',
     'positions',
     'ideaList',
