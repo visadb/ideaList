d2a2a6017bf55f3544ea3e473299d0b97b8ae549
Visa Putkinen <visa.putkinen@iki.fi>
2011-11-27 23:59:44 +0200

    Add some tests to ideaList models

--- ideaList/models.py
+++ ideaList/models.py
@@ -6,6 +6,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from positions.fields import PositionField
 from undelete.models import Trashable
+#from django.utils import unittest
+from django import test
 
 def _subscriptions_of_nontrashed_lists(self):
     return self.subscriptions.filter(list__trashed_at__isnull=True)
@@ -32,6 +34,23 @@ class List(Trashable):
         if self.trashed_at:
             val += " (trashed)"
         return val
+class ListTest(test.TestCase):
+    def setUp(self):
+        self.u = User.objects.create_user('pena', 'lol@lol.lol', 'passwd')
+        self.l1 = List.objects.create(name='List1', owner=self.u)
+    def test_fields(self):
+        self.assertTrue(List.objects.count() >= 1)
+        l = List.objects.all()[0]
+        self.assertEqual(l.name, 'List1')
+        self.assertEqual(l.owner, self.u)
+        self.assertEqual(l.subscribers.count(), 0)
+    def test_nontrashed_items(self):
+        self.assertEqual(self.l1.nontrashed_items().count(), 0)
+        i = Item.objects.create(list=self.l1, text="testitem")
+        self.assertEqual(self.l1.nontrashed_items().count(), 1)
+        self.assertEqual(self.l1.nontrashed_items()[0].text, 'testitem')
+        i.delete()
+        self.assertEqual(self.l1.nontrashed_items().count(), 0)
 
 class Item(Trashable):
     """
@@ -63,6 +82,19 @@ class Item(Trashable):
         if self.trashed_at:
             val += " (trashed)"
         return val
+class ItemTest(test.TestCase):
+    def setUp(self):
+        self.u = User.objects.create_user('pena', 'lol@lol.lol', 'passwd')
+        self.l1 = List.objects.create(name='List1', owner=self.u)
+        self.i1 = Item.objects.create(list=self.l1, text='testitem')
+    def test_fields(self):
+        self.assertTrue(Item.objects.count() >= 1)
+        i = Item.objects.all()[0]
+        self.assertEqual(i.list, self.l1)
+        self.assertEqual(i.text, 'testitem')
+        self.assertEqual(i.url, '')
+        self.assertEqual(i.priority, u'NO')
+        self.assertEqual(i.position, 0)
 
 class Subscription(models.Model):
     """
@@ -84,6 +116,18 @@ class Subscription(models.Model):
                 'position': self.position}
     def __unicode__(self):
         return self.user.first_name+": "+self.list.name
+class SubscriptionTest(test.TestCase):
+    def setUp(self):
+        self.u1 = User.objects.create_user('pena', 'lol@lol.lol', 'passwd')
+        self.l1 = List.objects.create(name='List1', owner=self.u1)
+        self.s = Subscription.objects.create(user=self.u1,list=self.l1)
+    def test_fields(self):
+        self.assertTrue(Subscription.objects.count() >= 1)
+        s = Subscription.objects.all()[0]
+        self.assertEqual(s.user, self.u1)
+        self.assertEqual(s.list, self.l1)
+        self.assertEqual(s.minimized, False)
+        self.assertEqual(s.position, 0)
 
 @receiver(post_save)
 def change_detect(sender, **kwargs):
@@ -106,3 +150,5 @@ class ChangeLog(models.Model):
     change_type = models.CharField(max_length=2, choices=CHANGE_TYPE_CHOICES)
     time = models.DateTimeField(db_index=True)
     user = models.ForeignKey(User, related_name='changes')
+class ChangeLogTest(test.TestCase):
+    pass
