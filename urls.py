f97966b25448a10107057d65cd664a77e899956f
Visa Putkinen <visa.putkinen@iki.fi>
2011-12-15 09:29:18 +0200

    Disable admin

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import redirect_to
 
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$',          redirect_to, {'url': '/ideaList/', 'permanent': False}),
@@ -12,5 +12,5 @@ urlpatterns = patterns('',
     url(r'^passwd/$',   'django.contrib.auth.views.password_change'),
     url(r'^passwd_done/$', 'django.contrib.auth.views.password_change_done'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/',     include(admin.site.urls)),
+    #url(r'^admin/',     include(admin.site.urls)),
 )
