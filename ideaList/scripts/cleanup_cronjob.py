623a7d581ff181ce16a2d118dca65d94af0d64b1
Visa Putkinen <visa.putkinen@iki.fi>
2012-02-15 23:37:21 +0200

    Clean old infrequent FrequentItems in cleaning cronjob

--- ideaList/scripts/cleanup_cronjob.py
+++ ideaList/scripts/cleanup_cronjob.py
@@ -5,7 +5,9 @@ from os import environ as env
 from datetime import datetime, timedelta
 sys.path.append('/srv/puhveli')
 env['DJANGO_SETTINGS_MODULE'] = 'settings'
-from ideaList.models import Item, List
+from ideaList.models import Item, List, ItemFrequency
+
+# Purge old deleted Lists and Items
 
 items_to_purge = Item.trash.filter(
         trashed_at__lt=datetime.now()-timedelta(days=7))
@@ -14,3 +16,8 @@ lists_to_purge = List.trash.filter(
 
 items_to_purge.delete()
 lists_to_purge.delete()
+
+# Purge old infrequent ItemFrequencies
+
+ItemFrequency.objects.filter(frequency__lte=1,
+        last_changed__lt=datetime.now()-timedelta(days=180)).delete()
