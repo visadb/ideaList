8bfb8f57f9fe0f5ec0a39ad4a0cbb3fedf86d17d
Visa de Bruijn <visa.de.bruijn@iki.fi>
2017-07-05 16:35:10 +0300

    Really fix item delete

--- undelete/models.py
+++ undelete/models.py
@@ -4,13 +4,13 @@ import signals
 
 class NonTrashedManager(models.Manager):
     ''' Query only objects which have not been trashed. '''
-    def get_query_set(self):
-        query_set = super(NonTrashedManager, self).get_query_set()
+    def get_queryset(self):
+        query_set = super(NonTrashedManager, self).get_queryset()
         return query_set.filter(trashed_at__isnull=True)
 class TrashedManager(models.Manager):
     ''' Query only objects which have been trashed. '''
-    def get_query_set(self):
-        query_set = super(TrashedManager, self).get_query_set()
+    def get_queryset(self):
+        query_set = super(TrashedManager, self).get_queryset()
         return query_set.filter(trashed_at__isnull=False)
 
 
